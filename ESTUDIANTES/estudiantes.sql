3a10c8c71f32dd5f84e37813e34a50877ac5617a
Thomasrr29 <thomasrr29@gmail.com>
2024-02-27 14:13:48 -0500

    avance tabla

---
+++ ESTUDIANTES/estudiantes.sql
@@ -0,0 +1,25 @@
+use bo57p3njkuyfdd3bqpe1
+
+CREATE TABLE estudiantes (id INT PRIMARY KEY AUTO_INCREMENT, nombre VARCHAR(45), apellidos VARCHAR(45), fecha_nacimiento VARCHAR(45), grado VARCHAR(45));
+
+INSERT INTO estudiantes(nombre, apellidos, fecha_nacimiento, grado) VALUES 
+("Jhon", "Loreña", "2003-04-30", "19"),
+("Lupe", "Perez", "2001-08-23", "23"),
+("Sebas", "Guillermo", "2002-08-30", "19"),
+("Alex", "Cardona", "2004-05-30", "29"),
+("Fernando", "Cardona", "2001-08-30", "21"),
+("Gilberto", "Gomez", "2000-08-30", "63"),
+("Manuel", "Ortiz", "1999-08-30", "99")
+
+
+
+CREATE TABLE materias (id INT PRIMARY KEY AUTO_INCREMENT nombre, profesor)
+
+INSERT INTO materias (nombre, profesor) VALUES ("Desarrollo Software", "Juan Jose Ortiz"),
+("Habilidades para la vida", "Federico Gomez"),
+("Ingles", "Catalina Perez"),
+("Lenguaje", "Ximena Villada"),
+("Historia", "Gilberto Posada"),
+
+
+
